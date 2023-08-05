69d9871595ab7fea3a355b50287f7c54922814b1
chloe <chloeimbusch@gmail.com>
2023-08-05 13:00:41 -0400

    two opt change

--- src/approximations/two_opt.py
+++ src/approximations/two_opt.py
@@ -1,37 +1,69 @@
 import math
 
 class TwoOpt:
-    def __init__(self, cities, tour):
+    def __init__(self, cities):
         self.cities = cities
-        self.tour = tour
+        self.num_cities = len(cities)
+        self.tour = []
 
-    def cities_distance(self, city1, city2):
+    def cities_distance(self, city1, city2): 
         return math.sqrt((city1.x - city2.x)**2 + (city1.y - city2.y)**2)
 
-    def total_distance(self):
-        total = 0
-        for i in range(len(self.tour) - 1):
-            total += self.cities_distance(self.cities[self.tour[i]], self.cities[self.tour[i+1]])
+    def find_closest(self, current, others): 
+        closest = None
+        closest_dist = float('inf')
 
-        return total
-    
-    def swap(self, i, k):
-        new_tour = self.tour[:i] + self.tour[i:k+1][::-1] + self.tour[k+1:]
-        return new_tour
-    
-    def improve(self):
-        best = self.tour.copy() 
-        better = True
+        for next_city in others:
+            distance = self.cities_distance(current, self.cities[next_city])
+            if distance < closest_dist:
+                closest = next_city
+                closest_dist = distance
 
-        while better:
-            better = False
-            for i in range(1, len(self.cities) - 1):
-                for j in range(i + 1, len(self.cities)):
-                    new_tour = self.swap(i, j)
-                    new_dist = self.total_distance()
-                    if new_dist < self.total_distance():
-                        best = new_tour
-                        better = True
+        return closest, closest_dist
 
-        self.tour = best
+    def solve_greedy(self):
+        self.tour = [0]
+        others = list(range(1, self.num_cities))
+
+        while others:
+            current = self.cities[self.tour[-1]]
+            closest, _ = self.find_closest(current, [self.cities[i] for i in others])
+            self.tour.append(self.cities.index(closest))
+            others.remove(closest)
+
+        self.tour.append(self.tour[0])
         return self.tour
+
+    def two_opt_swap(self, tour, i, j):
+        new_tour = tour[:i] + tour[i:j + 1][::-1] + tour[j + 1:]
+        return new_tour
+
+    def two_opt(self, tour):
+        best_tour = tour
+        improved = True
+
+        while improved:
+            improved = False
+            for i in range(1, self.num_cities - 2):
+                for j in range(i + 1, self.num_cities):
+                    if j - i == 1:
+                        continue  # No improvement if indices are adjacent
+                    new_tour = self.two_opt_swap(tour, i, j)
+                    if self.calculate_tour_distance(new_tour) < self.calculate_tour_distance(best_tour):
+                        best_tour = new_tour
+                        improved = True
+
+            tour = best_tour
+
+        return best_tour
+
+    def calculate_tour_distance(self, tour):
+        distance = 0
+        for i in range(len(tour) - 1):
+            distance += self.cities_distance(self.cities[tour[i]], self.cities[tour[i + 1]])
+        return distance
+
+    def solve(self):
+        greedy_tour = self.solve_greedy()
+        optimized_tour = self.two_opt(greedy_tour)
+        return optimized_tour
